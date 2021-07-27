b69716afaebc1b01e55524f3a57f5c7739953d69
hulian <lian.hu@juniorchina.com>
2021-07-27 15:00:06 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Date/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Date from '@/components/Date'
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    Date
+  }
+}
+</script>
